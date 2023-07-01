c50ba7ec5c481041dcf1c906dd7eb9f4e9d73485
Bruno Albuquerque <brunoalb.ing@gmail.com>
2023-06-30 23:45:21 -0300

    adjust

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/provider'
-import type { AppProps } from 'next/app'
-import { ToastContainer } from 'react-toastify';
+import {ChakraProvider} from '@chakra-ui/provider'
+import type {AppProps} from 'next/app'
+import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {FallbackLoader} from "@/components/FallbackLoader";
-import {Suspense} from "react";
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <ChakraProvider >
-                  <ToastContainer
-                  />
-                      <Component {...pageProps} />
-      </ChakraProvider>
-  )
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <ChakraProvider>
+            <ToastContainer
+            />
+            <Component {...pageProps} />
+        </ChakraProvider>
+    )
 }
