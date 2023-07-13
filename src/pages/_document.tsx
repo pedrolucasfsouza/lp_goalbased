2710d19faee9177ca8ed0588fcf3ad6cb7eeec8e
Pedro Lucas <pedrolucasfsouza@gmail.com>
2023-07-13 15:17:19 -0300

    Update: Add google tag manager analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
+import Analytics from "@/components/Analytics";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-BR">
       <title>BOLETAAI - A Central de Inteligencia do Investidor</title>
       <Head />
       <body>
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
