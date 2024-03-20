5256030e0423cdfb163c38280c7a661f8e040a1e
Pedro Lucas <pedrolucasfsouza@gmail.com>
2024-03-20 14:50:47 -0300

    a

--- src/services/performance.ts
+++
@@ -1,9 +0,0 @@
-import {api} from "@/services/axios";
-
-export const listActives = async (params: { search?: string }) => {
-    return await api.get(`/performance`, {
-        params: params
-    }).catch(({response}) => {
-        return response
-    })
-}
